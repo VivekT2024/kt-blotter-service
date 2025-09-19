2282c31e41b47345bd29ac0b5eb2f493e229422d
Vivek Lenovo <babloo32123@gmail.com>
2025-09-19 13:44:59 +0300

    Refactoring 1

--- src/main/java/com/vivekt/kt/config/AppConfig.java
+++ src/main/java/com/vivekt/kt/config/AppConfig.java
@@ -4,14 +4,24 @@ import com.vivekt.kt.serice.DataProvider;
 import com.vivekt.kt.serice.impl.DataProviderHardCodedImpl;
 import com.vivekt.kt.serice.impl.DataProviderServiceBadImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public DataProvider dataProvider(){
-        //return new DataProviderServiceBadImpl();
+    @Profile("hard")
+    public DataProvider dataProviderHard(){
+
         return new DataProviderHardCodedImpl();
+    }
+
+    @Bean
+    @Profile("bad")
+    public DataProvider dataProviderBad(){
+        return new DataProviderServiceBadImpl();
+
     }
 }
