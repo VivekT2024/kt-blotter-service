17b43b8f3bb350dea08007ad182134ce9f67014b
Vivek Lenovo <babloo32123@gmail.com>
2025-09-19 11:43:04 +0300

    Refactored

--- src/main/java/com/vivekt/kt/controller/WebSocketController.java
+++ src/main/java/com/vivekt/kt/controller/WebSocketController.java
@@ -2,7 +2,7 @@ package com.vivekt.kt.controller;
 
 
 
-import com.vivekt.kt.model.Message;
+import com.vivekt.ktpp.datamodel.Order;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -12,9 +12,9 @@ public class WebSocketController {
 
     @MessageMapping("/send") // listens to /app/send
     @SendTo("/topic/messages") // sends to /topic/messages
-    public Message sendMessage(Message message) throws Exception {
+    public Order sendMessage(Order order) throws Exception {
         // Simulate processing delay
         Thread.sleep(50);
-        return message;
+        return order;
     }
 }
